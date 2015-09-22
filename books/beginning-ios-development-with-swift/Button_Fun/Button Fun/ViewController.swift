efcd0f57d10df1bd4f14ed038cd86a8da5ec6f16
Shearerbeard <shearerbeard@gmail.com>
2015-09-22 17:13:11 -0600

    book chapter 2

---
+++ books/beginning-ios-development-with-swift/Button_Fun/Button Fun/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Button Fun
+//
+//  Created by Michael Shearer on 9/22/15.
+//  Copyright (c) 2015 Shearerbeard.LLC. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
