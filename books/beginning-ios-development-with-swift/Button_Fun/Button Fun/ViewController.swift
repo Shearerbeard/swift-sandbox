674acf6d3ae9a7c461d01ce13a5bd0fc8488422d
Shearerbeard <shearerbeard@gmail.com>
2015-09-22 20:50:41 -0600

    storyboard tweaks for chapter 3

--- books/beginning-ios-development-with-swift/Button_Fun/Button Fun/ViewController.swift
+++ books/beginning-ios-development-with-swift/Button_Fun/Button Fun/ViewController.swift
@@ -15,9 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
 
     @IBAction func buttonPressed(sender: UIButton) {
-        let title = sender.titleForState(.Normal)
-        let plainText = "\(title) button pressed"
-        statusLabel.text = plainText
+        let title = sender.titleForState(.Normal)!
+        func titleOrDefault(titleText: String?) -> String {
+            return title ?? ""
+        }
+        let plainText = "\(titleOrDefault(title)) button pressed"
+        let styledText = NSMutableAttributedString(string: plainText)
+        let attributes = [
+            NSFontAttributeName: UIFont.boldSystemFontOfSize(statusLabel.font.pointSize)
+        ]
+        let nameRange = (plainText as NSString).rangeOfString(titleOrDefault(title))
+        styledText.setAttributes(attributes, range: nameRange)
+
+        statusLabel.attributedText = styledText
     }
 
 }
