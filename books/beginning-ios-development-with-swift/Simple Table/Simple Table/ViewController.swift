c671f7103a8414e5cc72287f02c824663b97b4a7
Shearerbeard <shearerbeard@gmail.com>
2015-10-07 22:03:18 -0600

    added star images to table

--- books/beginning-ios-development-with-swift/Simple Table/Simple Table/ViewController.swift
+++ books/beginning-ios-development-with-swift/Simple Table/Simple Table/ViewController.swift
@@ -8,7 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let dwarves  = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur", "Bombur"
+    ]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier) 
+        if cell == nil {
+            cell = UITableViewCell(
+                style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        let image = UIImage(named: "star")
+        cell!.imageView?.image = image
+        
+        let higlightedImage = UIImage(named: "star2")
+        cell?.imageView?.highlightedImage = higlightedImage
+        
+        cell!.textLabel?.text = dwarves[indexPath.row]
+        return cell!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
