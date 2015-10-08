58254b953e7229f3f3119495ba48c4e322c25855
Shearerbeard <shearerbeard@gmail.com>
2015-10-07 22:36:50 -0600

    simple table view with text and sub labels

--- books/beginning-ios-development-with-swift/Simple Table/Simple Table/ViewController.swift
+++ books/beginning-ios-development-with-swift/Simple Table/Simple Table/ViewController.swift
@@ -17,17 +17,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         "Oin", "Gloin", "Bifur", "Bofur", "Bombur"
     ]
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.rowHeight = 70
+    }
+    
     let simpleTableIdentifier = "SimpleTableIdentifier"
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dwarves.count
     }
     
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        
+        return indexPath.row == 0 ? nil : indexPath
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let rowValue = dwarves[indexPath.row]
+        let message = "You selected \(rowValue)"
+        
+        let controller = UIAlertController(title: "Row Selected", message: message, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "Yes I Did", style: .Default, handler: nil)
+        
+        controller.addAction(action)
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier) 
         if cell == nil {
             cell = UITableViewCell(
-                style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+                style: UITableViewCellStyle.Subtitle, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        if indexPath.row < 7 {
+            cell!.detailTextLabel?.text = "Mr Disney"
+        } else {
+            cell!.detailTextLabel?.text = "Mr Tolkien"
         }
         
         let image = UIImage(named: "star")
