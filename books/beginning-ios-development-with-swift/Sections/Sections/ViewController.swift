8ef5c2320d7c11a5a2d935073798cb1ba89dc521
Shearerbeard <shearerbeard@gmail.com>
2015-10-11 12:36:23 -0600

    ch8 section header complete

--- books/beginning-ios-development-with-swift/Sections/Sections/ViewController.swift
+++ books/beginning-ios-development-with-swift/Sections/Sections/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let sectionTableIdentifier = "SectionTableIdentifier"
+
+    var names: [String: [String]]!
+    var keys: [String]!
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.registerClass(UITableViewCell.self,
+                forCellReuseIdentifier: sectionTableIdentifier)
+
+        let path = NSBundle.mainBundle().pathForResource(
+            "sortednames", ofType: "plist")
+
+        let namesDict = NSDictionary(contentsOfFile: path!)
+        names = namesDict as! [String: [String]]
+        keys = (namesDict!.allKeys as! [String]).sort()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return keys.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = keys[section]
+        let nameSection = names[key]!
+        return nameSection.count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(sectionTableIdentifier, forIndexPath: indexPath)
+        let key = keys[indexPath.section]
+        let nameSection = names[key]!
+        cell.textLabel?.text = nameSection[indexPath.row]
+        
+        return cell
+    }
 
 }
 
